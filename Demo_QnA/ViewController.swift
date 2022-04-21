c6f5071e4394cc81ac19bbea5a70f76940211106
supitt <613.billpitt@gmail.com>
2022-04-21 10:14:14 +0800

    Initial Commit

---
+++ Demo_QnA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo_QnA
+//
+//  Created by wentsung su on 2022/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
